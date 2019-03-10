5b76e737817112684d44a2bef3a26b5732e58f72
wangqiang-h <948829164@qq.com>
2019-03-10 15:02:52 +0800

    Create queen_recursion.py

---
+++ eight_queen/queen_recursion.py
@@ -0,0 +1,31 @@
+def conflict(y, state):
+    x = len(state)
+    for position_x, position_y in enumerate(state):
+        if position_y == y or (x - position_x) == (y - position_y) or (x-position_x) == (position_y-y):
+            return True
+    return False
+
+
+# def queens(num=4, state=()):
+#     if len(state) == num -1:
+#         for i in range(num):
+#             if not conflict(i, state):
+#                 yield (i,)
+#     else:
+#         for i in range(num):
+#             if not conflict(i, state):
+#                 for result in queens(num, state + (i,)):
+#                     yield (i,) + result
+
+def queens(num=4, state=()):
+    if len(state) == num -1:
+        for i in range(num):
+            if not conflict(i, state):
+                yield state + (i,)
+    else:
+        for i in range(num):
+            if not conflict(i, state):
+                yield from queens(num, state + (i,))
+print(list(queens(4)))
+
+
